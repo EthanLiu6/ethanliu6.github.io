bc8feb52ac0d089ef06bcf4d9d8047e1c5943be7
EthanLiu6 <liue2336@gmail.com>
2025-11-29 14:12:00 +0800

    add: coderethan博客迁移

---
+++ static/coderethan.fun/assets/AI_01_deep_learning_theory_index.md.DULtN52e.lean.js
@@ -0,0 +1,33 @@
+import{_ as a,c as n,o as i,a2 as e}from"./chunks/framework.DA-Pb-tg.js";const c=JSON.parse('{"title":"Deep Learning Theroy","description":"","frontmatter":{},"headers":[],"relativePath":"AI/01_deep_learning_theory/index.md","filePath":"AI/01_deep_learning_theory/index.md","lastUpdated":1743069065000}'),p={name:"AI/01_deep_learning_theory/index.md"};function l(t,s,h,r,k,d){return i(),n("div",null,s[0]||(s[0]=[e(`<h1 id="deep-learning-theroy" tabindex="-1">Deep Learning Theroy <a class="header-anchor" href="#deep-learning-theroy" aria-label="Permalink to &quot;Deep Learning Theroy&quot;">​</a></h1><p>Summary of deep learning theory<br> 总共47个章节，为了阅读美观，去掉的对应的文件夹，直接整理成文档内容（按序）</p><div class="language-shell vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">shell</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">01-feedforward_network/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">02-back_propagation/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">03-bp_example_demo/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">04-convolution_neural_network/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">05-deep_learning_model/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">06-pytorch_install/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">07-operators/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">08-activation_functions/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">09-recurrent_neural_network/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">10-seq2seq/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">11-attentions/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">12-weight-initialization/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">13-optimizers/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">14-regularization/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">15-deep-learning-tuning-guide/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">20-pytorch-tensor/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">21-pytorch-autograd/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">22-pytorch-module/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">23-training-example-1/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">24-pytorch-optimizer/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">25-pytorch-lr-scheduler/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">26-pytorch-dataloader/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">27-pytorch-model-save/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">28-pytorch-tensorboard/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">29-pytorch-graph-mode/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">30-training-example-2/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">40-nlp-bert/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">41-nlp-t5/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">42-stable-diffusion/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">44-scaling-law/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">45-distribute-training/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">46-nlp-llama/</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">47-nlp-deepseek/</span></span></code></pre></div><hr><p>后续有过修改</p>`,5)]))}const F=a(p,[["render",l]]);export{c as __pageData,F as default};
